cabdddcf2a4e9da23e8d2e888ff9d67cc303c684
Carlos Rodriguez <carlos@s8f.org>
2012-05-20 21:33:37 -0700

    Add ship

--- client/lights.js
+++ client/lights.js
@@ -2,11 +2,11 @@ define(function() {
   var lights = [], light;
   // lights
   light = new THREE.DirectionalLight( 0xffffff );
-  light.position.set( 1, 1, 1 );
+  light.position.set( 20, 50, 400 );
   lights.push(light);
 
   light = new THREE.DirectionalLight( 0xffffff );
-  light.position.set( -1, -1, -1 );
+  light.position.set( -20, -50, -400 );
   lights.push(light);
 
   light = new THREE.AmbientLight( 0x222222 );
--- client/scene.js
+++ client/scene.js
@@ -1,6 +1,8 @@
-define(['lights', 'stars'], function(lights, stars) {
+define(['camera', 'lights', 'stars', 'ship'], function(camera, lights, stars, getShip) {
   var scene = new Physijs.Scene({ reportsize: 200, fixedTimeStep: 1 / 30 });
   scene.setGravity(new THREE.Vector3( 0, 0, 0 ));
+
+  scene.add(camera);
 
   // Add lights
   for (var i in lights) {
@@ -9,6 +11,11 @@ define(['lights', 'stars'], function(lights, stars) {
 
   // Add stars
   scene.add(stars);
+
+  // Add ship
+  getShip(function(ship) {
+    scene.add(ship);
+  });
 
   return scene;
 });
