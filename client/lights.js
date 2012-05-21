72387495cd12d7053271c4c2929797067bdfc7f4
Carlos Rodriguez <carlos@s8f.org>
2012-05-20 23:36:58 -0700

    Lighten up ship

--- client/lights.js
+++ client/lights.js
@@ -2,14 +2,14 @@ define(function() {
   var lights = [], light;
   // lights
   light = new THREE.DirectionalLight( 0xffffff );
-  light.position.set( 20, 50, 400 );
+  light.position.set( 1000, 5000, 5000 );
   lights.push(light);
 
   light = new THREE.DirectionalLight( 0xffffff );
-  light.position.set( -20, -50, -400 );
+  light.position.set( -1000, -5000, -5000 );
   lights.push(light);
 
-  light = new THREE.AmbientLight( 0x222222 );
+  light = new THREE.AmbientLight( 0x555555 );
   lights.push(light);
   return lights;
 });
