f82798e81fbb1a82ed96a136abca2c196bb41cb5
Carlos Rodriguez <carlos@s8f.org>
2012-05-20 23:31:11 -0700

    Fix stars dropping off

--- client/camera.js
+++ client/camera.js
@@ -1,5 +1,5 @@
 define(function() {
-  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 20000);
   camera.position.z = 20;
   camera.position.y = 5;
   return camera;
